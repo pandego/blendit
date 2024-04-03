508aee68ab7d5627c287120354f1d8ad02bb969b
pandego <7780875+pandego@users.noreply.github.com>
2024-04-03 22:00:30 +0200

    [WIP][FEAT] Update model upload script, Python version and model paths

--- upload_model_to_hf.sh
+++ upload_model_to_hf.sh
@@ -1,20 +1,12 @@
 # --- 🚧 UNDER CONSTRUCTION 🚧 --- #
 # This script is a work in progress and is not yet functional
-# In summary, the script will upload a model to the Hugging Face model hub
-# The script will require the user to have an access token and write permission to the Hugging Face model hub
-# The script will also require the user to have the Hugging Face CLI installed
-# The script will require the user to have a Hugging Face account
-# The script will require the user to have a model directory to upload
-# The script will require the user to have a Hugging Face username
-# The script will require the user to have a model name
 
-# Get HF_TOKEN from User
-read -p "Please enter your HuggingFace Token: " HF_TOKEN
-read -p "Please enter your HuggingFace Username: " HF_USERNAME
-read -p "Please enter your Model's name: " MODEL_NAME
+# Get info from User
+read -p "Eh, what's up, Doc? Please enter your HuggingFace Token: " HF_TOKEN
+read -p "And your HuggingFace Username, Doc: " HF_USERNAME
+read -p "What's the name of your Model, Doc? " MODEL_NAME
+read -p "Where are the merged model stored, Doc? " MODELS_DIR
 
-# log in to huggingface with an access token (must have write permission)
-huggingface-cli login --token $HF_TOKEN
-
-# upload your model
-huggingface-cli upload --repo-type model $HF_USERNAME/$MODEL_NAME models/merged-models/$MODEL_NAME
+# Upload your model
+# huggingface-cli upload --repo-type model $HF_USERNAME/$MODEL_NAME models/merged-models/$MODEL_NAME .
+huggingface-cli upload --repo-type model --token $HF_TOKEN $HF_USERNAME/$MODEL_NAME $MODELS_DIR .
